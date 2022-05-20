9d414982f38723630b4a1d827ebc3434dd320c88
Martin Mueller <martin.ernst.mueller@gmail.com>
2022-05-20 14:20:54 +0200

    First implementation

---
+++ Controllers/KaffeeController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace KaffeeMaschine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KaffeeController : ControllerBase
+    {
+        Kaffee kaffee = new Kaffee(1.2, 0.8);
+
+        private readonly ILogger<KaffeeController> _logger;
+
+        public KaffeeController(ILogger<KaffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        [Route("Wasser")]
+        public double GetWasserstand()
+        {
+            return kaffee.wasser;
+        }
+
+
+        [HttpGet()]
+        [Route("Bohnen")]
+        public double GetBohnenFüllstand()
+        {
+            return kaffee.bohnen;
+        }
+
+        [HttpPut()]
+        [Route("PutBohnen")]
+
+        public double PutBohnen(double menge)
+        {
+            return kaffee.bohnenAuffuellen(menge);
+
+        }
+
+        [HttpPut()]
+
+        [Route("PutWasser")]
+        public double PutWasser(double menge)
+        {
+            return kaffee.wasserAuffuellen(menge);
+
+        }
+
+        [HttpPut()]
+
+        [Route("MachKaffee")]
+        public IActionResult MachKaffee(double menge, double verhaeltnisWasserBohnen)
+        {
+            var retval = kaffee.machKaffee(menge, verhaeltnisWasserBohnen);
+            if (retval == menge)
+            {
+                return Ok("Restwasser: " + kaffee.wasser + ". Restbohnen: " + kaffee.bohnen + ".");
+            }
+
+            return Conflict("Not enough water or beens... please fill up first!");
+
+        }
+
+
+    }
+}
---
+++ Kaffee.cs
@@ -0,0 +1,50 @@
+namespace KaffeeMaschine
+{
+    public class Kaffee
+    {
+        public double wasser { get; private set; }
+        public double bohnen { get; private set; }
+        private double maxWasser = 2.5;
+        private double maxBohnen = 2.5;
+
+
+        public Kaffee(double argBohnen, double argWasser)
+        {
+            argBohnen = (argBohnen > maxBohnen) ? 2.5 :  argBohnen;
+            argWasser = (argWasser > maxWasser) ? maxWasser : argWasser;
+            bohnen = argBohnen;
+            wasser = argWasser;
+        }
+
+        public double wasserAuffuellen(double menge)
+        {
+            wasser = ((wasser + menge) > maxWasser) ? maxWasser : wasser + menge;
+            return wasser;
+        }
+
+        public double bohnenAuffuellen(double menge)
+        {
+            bohnen = ((bohnen + menge) > maxBohnen) ? maxBohnen : bohnen + menge;
+            return bohnen;
+        }
+
+
+        public double machKaffee(double menge, double verhaeltnisWasserBohnen)
+        {
+            var mengeWasser = menge / 2 * verhaeltnisWasserBohnen;
+            var mengeBohnen = menge / 2 * (1/verhaeltnisWasserBohnen);
+            if (wasser < mengeWasser)
+                return -1.0;
+            if (bohnen < mengeBohnen)
+                return -2.0;
+
+            wasser -= mengeWasser;
+            bohnen -= mengeBohnen;
+
+            return menge;
+        }
+
+
+
+    }
+}
