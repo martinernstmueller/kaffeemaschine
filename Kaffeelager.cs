1583cbf42105abaf49452ed3aaf4e970c964d0b2
botrick01 <johannsenpatrick.privat@gmail.com>
2022-06-10 15:07:16 +0200

    KaffeelagerController added

---
+++ Kaffeelager.cs
@@ -0,0 +1,12 @@
+﻿namespace KaffeeMaschine
+{
+    public class Kaffeelager
+    {
+        public double lagerstand { get; set; }
+
+        public Kaffeelager (double _lagerstand)
+        {
+            lagerstand = _lagerstand;
+        }
+    }
+}
