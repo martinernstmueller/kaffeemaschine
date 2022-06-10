79391da665661ec2b6127f513f613bc0c23f31fe
ShortyDev <shortydev8274@gmail.com>
2022-06-10 13:23:42 +0200

    kaffeelager

---
+++ Controllers/KaffeelagerController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace KaffeeMaschine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KaffeeController : ControllerBase
+    {
+        static Kaffee kaffee = new Kaffee(1,1);
+
+        private readonly Kaffeelager kaffeelager;
+        private readonly ILogger<KaffeeController> _logger;
+
+        public KaffeeController(ILogger<KaffeeController> logger, Kaffeelager kaffeelager)
+        {
+            _logger = logger;
+            this.kaffeelager = kaffeelager;
+
+        }
+
+        [HttpGet()]
+        [Route("Wasser")]
+        public double GetWasserstand()
+        {
+            return kaffee.wasser;
+        }
+
+
+        [HttpGet()]
+        [Route("Bohnen")]
+        public double GetBohnenFüllstand()
+        {
+            return kaffee.bohnen;
+        }
+
+        [HttpPut()]
+        [Route("PutBohnen")]
+
+        public double PutBohnen(double menge)
+        {
+            kaffeelager.bohnenEntnehmen(menge);
+            return kaffee.bohnenAuffuellen(menge);
+
+        }
+
+        [HttpPut()]
+
+        [Route("PutWasser")]
+        public double PutWasser(double menge)
+        {
+            kaffeelager.wasserEntnehmen(menge);
+            return kaffee.wasserAuffuellen(menge);
+
+        }
+
+        [HttpPut()]
+
+        [Route("MachKaffee")]
+        public IActionResult MachKaffee(double menge, double verhaeltnisWasserBohnen)
+        {
+            var retval = kaffee.machKaffee(menge, verhaeltnisWasserBohnen);
+            if (retval == menge)
+            {
+                return Ok("Restwasser: " + kaffee.wasser + ". Restbohnen: " + kaffee.bohnen + ".");
+            }
+
+            return Conflict("Not enough water or beens... please fill up first!");
+
+        }
+
+
+    }
+}
