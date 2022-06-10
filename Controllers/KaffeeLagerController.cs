e82a48bff45174f40ebc676c974eeadbe934fc06
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2022-06-10 15:44:18 +0200

    Kaffeelager controller

---
+++ Controllers/KaffeeLagerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KaffeeMaschine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KaffeelagerController : ControllerBase
+    {
+        KaffeeLager Lager;
+        private readonly ILogger<KaffeelagerController> _logger;
+
+        public KaffeelagerController(ILogger<KaffeelagerController> logger, KaffeeLager argLager)
+        {
+            _logger = logger;
+            Lager = argLager;
+        }
+
+        [HttpGet, Route("LagerInhalt")]
+        public double Lagerinhalt()
+        {
+            return Lager.fuellstand;
+        }
+
+        [HttpPut, Route("ChangeLager")]
+        public double ChangeLagerinhalt(double menge)
+        {
+            return Lager.fuellstand += menge;
+        }
+    }
+}
--- KaffeeLager.cs
+++ KaffeeLager.cs
@@ -9,6 +9,5 @@ namespace KaffeeMaschine
             fuellstand = argFuellstand;
          }
 
-     
     }
 }
